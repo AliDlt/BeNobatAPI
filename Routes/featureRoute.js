4ea63dc2adf8bc0ba39a88fe298f135678e43df7
AliDlt <alidlt1@yahoo.com>
2024-01-21 21:20:21 +0330

    update models

--- Routes/featureRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const featureController = require("./featureController");
-
-// Endpoint to update feature flags
-router.post("/update-feature-flags", featureController.updateFeatureFlags);
-
-module.exports = router;
